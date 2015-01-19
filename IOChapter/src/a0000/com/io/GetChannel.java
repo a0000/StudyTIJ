853b89f86fac0f16a5b2a9457e09a34d34fc56c6
a0000 <qyxueli@163.com>
2015-01-20 00:03:31 +0800

    Rejected commit: Default

---
+++ IOChapter/src/a0000/com/io/GetChannel.java
@@ -0,0 +1,35 @@
+package a0000.com.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by Administrator on 2015/1/19.
+ */
+public class GetChannel {
+    private static final int BISIZE = 1024;
+
+    public static void main(String[] args) throws IOException{
+        // Write a file;
+        FileChannel fc = new FileOutputStream("data.txt").getChannel();
+        fc.write(ByteBuffer.wrap("Some text ".getBytes()));
+        fc.close();
+        // Add to the end of the file;
+        fc = new RandomAccessFile("data.txt", "rw").getChannel();
+        fc.position(fc.size()); // Move to the end
+        fc.write(ByteBuffer.wrap("Some more ".getBytes()));
+        fc.close();
+        // Read the file;
+        fc = new FileInputStream("data.txt").getChannel();
+        ByteBuffer buff = ByteBuffer.allocate(BISIZE);
+        fc.read(buff);
+        buff.flip();
+        while (buff.hasRemaining()) {
+            System.out.print((char)buff.get());
+        }
+    }
+}

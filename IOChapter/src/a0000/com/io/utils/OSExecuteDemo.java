9b31bb23ac47d710c3c309bc34721c8fa42f4364
a0000 <qyxueli@163.com>
2015-01-19 22:32:55 +0800

    Rejected commit: Default

---
+++ IOChapter/src/a0000/com/io/utils/OSExecuteDemo.java
@@ -0,0 +1,10 @@
+package a0000.com.io.utils;
+
+/**
+ * Created by Administrator on 2015/1/18.
+ */
+public class OSExecuteDemo {
+    public static void main(String[] args) {
+        OSExecute.command("java -version");
+    }
+}

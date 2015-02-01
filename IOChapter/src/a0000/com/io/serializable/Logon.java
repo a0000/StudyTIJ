d6ec8601b1688ad4547dd151f5e2b54f7b80bda4
a0000 <qyxueli@163.com>
2015-02-02 01:22:24 +0800

    Rejected commit: Default

--- IOChapter/src/a0000/com/io/serializable/Logon.java
+++ IOChapter/src/a0000/com/io/serializable/Logon.java
@@ -1,7 +1,42 @@
 package a0000.com.io.serializable;
 
+import java.io.*;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static a0000.com.io.utils.Print.*;
+
 /**
  * Created by Administrator on 2015/2/2.
+ * Demonstrates the "transient" keyword.
  */
-public class Logon {
+public class Logon implements Serializable {
+    private static final long serialVersionUID = -6090484425782957496L;
+    private Date date = new Date();
+    private String username;
+    private transient String password;
+    public Logon(String name, String pwd) {
+        username = name;
+        password = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "logon info: \n  username: " + username +
+                "\n  date: " + date + "\n password: " + password;
+    }
+
+    public static void main(String[] args)throws Exception{
+        Logon a = new Logon("Hulk", "myLittlePony");
+        print("logon a = " + a);
+        ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream("Logon.out"));
+        o.writeObject(a);
+        o.close();
+        TimeUnit.SECONDS.sleep(1); // Delay
+        // Now get them back:
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("Logon.out"));
+        print("Recovering object at " +new Date());
+        a = (Logon)in.readObject();
+        print("logon a = " + a);
+    }
 }
